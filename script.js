6ee0e7917e728a1aa65970d896c45702e70729e1
yoelng <yoelgoitom25@gmail.com>
2021-02-08 00:28:47 -0800

    changing DIV with eventListner

---
+++ script.js
@@ -0,0 +1,25 @@
+const button1 = document.getElementById("btn1");
+const button2 = document.getElementById("btn2");
+const button3 = document.getElementById("btn3");
+const button4 = document.getElementById("btn4");
+
+button1.addEventListener("click", btnObj);
+button2.addEventListener("click", btnSkill);
+button3.addEventListener("click", btnInterest);
+button4.addEventListener('click',btnExperience)
+
+function btnObj() {
+  document.getElementById("obj").style.backgroundColor = "brown";
+}
+
+function btnSkill() {
+  document.getElementById("allSkill").innerHTML = "Computer skills";
+}
+
+function btnInterest() {
+  document.getElementById("int").innerText = "YOU JUST MISSED MY INTERESTS";
+}
+
+function btnExperience(){
+    document.
+}
